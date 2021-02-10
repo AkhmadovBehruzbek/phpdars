18a2a764614ffd0652913f3554ff642de556b09c
AkhmadovBehruzbek <behruzbekakhmadov@gmail.com>
2021-02-10 12:21:55 +0500

    form ism va yoshni javob php ga yuboruvchi

--- dars1/index.php
+++ dars1/index.php
@@ -1,3 +1,16 @@
-<?php
-    echo  "Hello World";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1- dars</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="ism" placeholder="Ismingiz?"><br>
+        <input type="number" name="yosh" placeholder="Yoshingiz?"><br>
+        <input type="submit" name="submit" value="Yubor">
+    </form>
+</body>
+</html>
