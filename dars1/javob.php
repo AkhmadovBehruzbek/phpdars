fade13d8d614fcdd8751a19abc62b4eec6f9d463
AkhmadovBehruzbek <behruzbekakhmadov@gmail.com>
2021-02-10 12:32:53 +0500

    ism va yosh

--- dars1/javob.php
+++ dars1/javob.php
@@ -1,7 +1,7 @@
 <?php
     $ism = $_GET['ism'];
     $yosh = $_GET['yosh'];
-    echo "<h2>{$ism}</h2>";
-    echo "<h2>{$yosh}</h2>";
+    echo "<h2>Ismingiz: {$ism}</h2>";
+    echo "<h2>Yoshingiz: {$yosh}</h2>";
 
 ?>
