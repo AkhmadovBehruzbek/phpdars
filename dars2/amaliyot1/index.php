95b2e62bdff5dcb969e2282f7b3e2d2ab76c159f
AkhmadovBehruzbek <behruzbekakhmadov@gmail.com>
2021-02-10 22:25:42 +0500

    valyuta ayriboshlash dars bitta gaylda

--- dars2/amaliyot1/index.php
+++ dars2/amaliyot1/index.php
@@ -20,5 +20,24 @@
         <input type="submit" name="submit" value="Ayriboshlash"><br>
 
     </form>
+
+    <?php
+    $son = $_GET['miqdor'];
+    $tip = $_GET['radio'];
+    $index = 'index.php';
+
+    if (isset($_GET['submit'])) {
+        if ($tip == "s") {
+            $n = $son * 8300;
+            echo number_format($n,0,""," ") . " so'm";
+        }
+        else {
+            $n = $son / 8300;
+            echo number_format($n,2,".", " ") . " $";
+        }
+    }
+
+?>
+
 </body>
 </html>
