b4fd740f47143ff106f1075ab8d54df7fd45ef40
AkhmadovBehruzbek <behruzbekakhmadov@gmail.com>
2021-02-10 20:46:39 +0500

    Ikkinchi dars maxsulot narxini aniqlash shart operatorlari

---
+++ dars2/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2- dars</title>
+</head>
+<body>
+    <h2>2 - dars shart operatorlari</h2>
+    <p>1- qism mahsulotni sonini va narxini kiritganimizda mahsulot narxini hisoblaydigan dastur.</p><br>
+    <p>2 - qism agar mahsulot narxi 50 000 dan katta bo'lsa 10% chegirma qiluvchi dastur.</p>
+
+    <form action="test.php" method="GET">
+
+        <input type="number" name="narx" placeholder="Maxsulot narxini kiriting:">
+
+        <select name="son" id="">
+            <option >1</option>
+            <option >2</option>
+            <option >3</option>
+            <option >4</option>
+        </select>
+        <br>
+        
+        <input type="submit" name="submit" value="natija">
+    </form>
+</body>
+</html>
---
+++ dars2/test.php
@@ -0,0 +1,32 @@
+<?php
+
+    $son = $_GET['son'];
+    $narx = $_GET['narx'];
+    $index = 'index.php';
+
+    $summ = $son * $narx;
+
+    echo "Maxsulot narxi: {$summ} so'm<br>";
+
+    if ($summ >= 50000) {
+        $ch = $summ * 0.1;
+        $t = $summ - $ch;
+
+        echo "Siz uchun chegirma 10%: {$ch}<br>";
+
+        echo "To'lash kerak: {$t}<br>";
+    } elseif ($summ >= 30000) {
+        $ch = $summ * 0.05;
+        $t = $summ - $ch;
+
+        echo "Siz uchun chegirma 5%: " .$ch. '<br>';
+        echo "To'lash kerak: " .$t. '<br>';
+    }
+     else {
+        echo "Chegirma mavjud emas <br>";
+        
+    }
+
+    echo "<a href='{$index}'>Orqaga qaytish</a>";
+
+?>
