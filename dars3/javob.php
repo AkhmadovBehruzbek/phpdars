ed2ffcf237e199eaa8f78b77ce735f53f43e248d
AkhmadovBehruzbek <behruzbekakhmadov@gmail.com>
2021-02-15 21:22:38 +0500

    php da fayllar bilan ishlash 1_qism

--- dars3/javob.php
+++ dars3/javob.php
@@ -4,10 +4,18 @@
     echo $_FILES['f']['type'] . '<br>';
     echo $_FILES['f']['tmp_name'] . '<br>';
     echo $_FILES['f']['error'] . '<br>';
-    $b = $_FILES['f']['size'];
-    echo $b / 1024 . " kb";
+    $b = $_FILES['f']['size'] / 1024;
+    echo $b . " kb <br>";
+    echo rand(0, 2500) ."<br>";
 
     
-    move_uploaded_file($_FILES["f"]["tmp_name"],"uploads/" . $_FILES["f"]["name"]);
+?>
 
+<img src="'uploads/'<?php echo $_FILES['f']['name']; ?>" alt="rasm">
+<?php
+if ($b >= 2000) {
+    echo "katta fayl";
+} else {
+    move_uploaded_file($_FILES["f"]["tmp_name"],"uploads/" . $_FILES["f"]["name"]);
+}
 ?>
