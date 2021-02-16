119d00ba36c3960531939333aeaa6576b9e083cf
AkhmadovBehruzbek <behruzbekakhmadov@gmail.com>
2021-02-16 15:50:45 +0500

    Amaliyot 3_2 rasm kiritlayotkanda unga nom berish

--- dars3/amaliyot_3_2/index.php
+++ dars3/amaliyot_3_2/index.php
@@ -10,7 +10,24 @@
     <h2>3_2 amaliy ish</h2>
     <p>Rasm yuklanganda forma orqali ism kiritladi va rasm serverga kiritilgan ism nomi bilan saqlanadi va ekranga chiqariladi.
     </p>
-
     
+    <form action="" method="POST" enctype="multipart/form-data">
+        <input type="text" name="ism" placeholder="Ismingizni kiriting:"><br>
+        <input type="file" name="file"><br>
+        <input type="submit" value="yuborish" name="submit"><br>
+    </form>
+
+    <?php
+        if (isset($_POST['submit'])) {
+            $ism = $_POST['ism'];
+            if ($_FILES['file']['type'] == "image/jpeg") {
+                $y = "uploads/".$ism.".jpg";
+                move_uploaded_file ($_FILES['file']['tmp_name'], $y);
+            } else {
+                echo "Fayl tipi boshqa";
+            }
+        }
+    ?>
+
 </body>
 </html>
