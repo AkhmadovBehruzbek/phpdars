0693cda96c742435d9275b16bd2f740d1d24d79c
AkhmadovBehruzbek <behruzbekakhmadov@gmail.com>
2021-02-17 23:06:08 +0500

    while loopi

---
+++ dars3/looplar/while.php
@@ -0,0 +1,16 @@
+<?php
+    // while operatori
+$i = 1;
+    while($i<10) {
+        echo $i . ")";
+        echo " salom<br>";
+        $i++;
+    }
+
+    // do while operatori
+$k = 1;
+    do {
+        echo "salom<br>";
+        $k++;
+    } while ($k<10);
+?>
