22fa444ad768024e1f4e7990a0dbec3a345f6432
akhmadov <akhmadov@DESKTOP-609J7CG.localdomain>
2021-03-07 10:55:16 +0500

    a

--- dars8_base/amaliyot_8_1/index.php
+++ dars8_base/amaliyot_8_1/index.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Amaliyot 1</title>
 </head>
+
 <body>
     <h1>Amaliyot 8_1</h1>
-    <p>'sayt' nomli baza yaratamiz, 'menu' nomli jadval va jadval 'id, menu_nomi, izoh, vaqt' nomli qatorlardan iborat bo'ladi. 
-    bazani sahifaga bog'lang va sahifadan ma'lumot kiritib bazani to'ldirish imkoniyatiga egab bolish lo</p>
+    <p>'sayt' nomli baza yaratamiz, 'menu' nomli jadval va jadval 'id, menu_nomi, izoh, vaqt' nomli qatorlardan iborat
+        bo'ladi.
+        bazani sahifaga bog'lang va sahifadan ma'lumot kiritib bazani to'ldirish imkoniyatiga egab bolish lo</p>
 
     <form action="" method="GET">
         <label for="menu_name">Menyu nomini kiriting:</label>
@@ -37,6 +40,8 @@
             }
             
         }
+        echo "hello world";
     ?>
 </body>
+
 </html>
